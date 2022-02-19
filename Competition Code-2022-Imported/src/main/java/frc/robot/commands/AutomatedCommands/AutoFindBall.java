e79d32d1408008a87f30a9bc343821065bed86a0
Moah700 <noah.fern007@gmail.com>
2022-02-19 03:55:48 -0600

    update

--- Competition Code-2022-Imported/src/main/java/frc/robot/commands/AutomatedCommands/AutoFindBall.java
+++ Competition Code-2022-Imported/src/main/java/frc/robot/commands/AutomatedCommands/AutoFindBall.java
@@ -1,23 +1,32 @@
 package frc.robot.commands.AutomatedCommands;
 
 import frc.robot.subsystems.Vision;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
+import frc.robot.commands.AutomatedCommands.*;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class AutoFindBall extends SequentialCommandGroup {
-  private Vision vision;
-  private SwerveRotaters rotaters;
-  private SwerveSpinners spinners;
-  private Gyro gyro;
+public class AutoFindBall extends SequentialCommandGroup{
+    private Vision vision;
+    private SwerveRotaters rotaters;
+    private SwerveSpinners spinners;
+    private Gyro gyro;
+    private Intake intake;
 
-  public AutoFindBall(Vision vision, SwerveRotaters rotaters, SwerveSpinners spinners, Gyro gyro) {
-    this.vision = vision;
-    this.rotaters = rotaters;
-    this.spinners = spinners;
-    this.gyro = gyro;
+    public AutoFindBall(Vision vision,SwerveRotaters rotaters,
+    SwerveSpinners spinners,Gyro gyro,Intake intake)
+    {
+        this.vision = vision;
+        this.rotaters = rotaters;
+        this.spinners = spinners;
+        this.gyro = gyro;
+        this.intake = intake;
 
-    addCommands(
-        new AutoVisionRotate(vision, rotaters, spinners, gyro),
-        new AutoVisionDrive(vision, rotaters, spinners, gyro));
-  }
+        addCommands( 
+            new AutoVisionRotate(vision, rotaters, spinners, gyro),
+            new AutoVisionDriveAndIntake(vision, rotaters, spinners, gyro, intake));
+        
+    }
+
+    
 }
--- Competition Code-2022-Imported/src/main/java/frc/robot/commands/AutomatedCommands/AutoVisionDriveAndIntake.java
+++ Competition Code-2022-Imported/src/main/java/frc/robot/commands/AutomatedCommands/AutoVisionDriveAndIntake.java
@@ -3,31 +3,36 @@ package frc.robot.commands.AutomatedCommands;
 import frc.robot.subsystems.Vision;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.*;
+import frc.robot.commands.AutomatedCommands.*;
 
-public class AutoVisionDrive extends CommandBase {
-  private Vision vision;
-  private SwerveRotaters rotaters;
-  private SwerveSpinners spinners;
-  private Gyro gyro;
-  private double fR, fL, bR, bL;
+public class AutoVisionDriveAndIntake extends CommandBase{
+    private Intake intake;
+    private Vision vision;
+    private SwerveRotaters rotaters;
+    private SwerveSpinners spinners;
+    private Gyro gyro;
+    private double fR, fL, bR, bL;
 
-  public AutoVisionDrive(
-      Vision vision, SwerveRotaters rotaters, SwerveSpinners spinners, Gyro gyro) {
-    this.vision = vision;
-    this.rotaters = rotaters;
-    this.spinners = spinners;
-    this.gyro = gyro;
-    this.fR = 0;
-    this.fL = 0;
-    this.bR = 0;
-    this.bL = 0;
-    addRequirements(rotaters, spinners);
-  }
+    public AutoVisionDriveAndIntake(Vision vision, SwerveRotaters rotaters, SwerveSpinners spinners, Gyro gyro, Intake intake)
+    {
+        this.vision = vision;
+        this.rotaters = rotaters;
+        this.spinners = spinners;
+        this.gyro = gyro;
+        this.fR = 0;
+        this.fL = 0;
+        this.bR = 0;
+        this.bL = 0;
+        this.intake = intake;
+        addRequirements(rotaters, spinners);
+    }
 
-  // Called when the command is initially scheduled.
+    // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     rotaters.setWheelDirection(fR, fL, bR, bL);
+    intake.pistonForward();
+    intake.intake();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -44,12 +49,13 @@ public class AutoVisionDrive extends CommandBase {
   public void end(boolean interrupted) {
     rotaters.stop();
     spinners.stop();
+    intake.off();
     System.out.println("Finished");
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return vision.getArea() > 80.0;
+    return vision.getArea()>80.0;
   }
 }
